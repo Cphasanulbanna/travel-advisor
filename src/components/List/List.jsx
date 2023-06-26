e7a510a0339cbfd4447a8aed12507e450b29783c
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-06-26 23:14:22 +0530

    added google map

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 
+//packages
+import {
+    CircularProgress,
+    Grid,
+    Typography,
+    InputLabel,
+    MenuItem,
+    FormControl,
+    Select,
+} from "@material-ui/core";
+
 const List = () => {
     return <div>List</div>;
 };
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,35 @@
 import React from "react";
 
+//packages
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import { Rating } from "@material-ui/lab";
+
+//icons
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
+
+//css
+import useStyles from "./styles";
+
 const Map = () => {
-    return <div>Map</div>;
+    const classes = useStyles();
+    const isMobile = useMediaQuery("(min-width: 600px)");
+
+    const coordinates = { lat: 0, lng: 0 };
+    return (
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: import.meta.env.VITE_GOOGLE_MAP_APIKEY }}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50, 50, 50, 50]}
+                // options={""}
+                // onChange={""}
+                // onChildClick={""}
+            ></GoogleMapReact>
+        </div>
+    );
 };
 
 export default Map;
